15a3a1cc96274aada7442d7a28ec33f8e36e94f8
#Fernangel <105268105+Fernangel7@users.noreply.github.com>
2025-09-12 11:14:09 -0700

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,10 @@
+#calificaciones
+luis_matrix = 4
+luis_frozen = 2
+
+#calificaciones ana
+ana_matrix = 5
+ana_frozen = None
+
+#Paso #1 calcular similitud entre ana y luis
+numerador = ana_matrix * luis_matrix
