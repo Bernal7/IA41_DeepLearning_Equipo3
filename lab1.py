cc53fdbf90766641d263785c85eb9bc09f22b5f2
Angel Bernal <tic-320030@utnay.edu.mx>
2025-09-12 22:19:46 -0700

    Jose Angel Commit's
    I wrote the 3rd step

--- lab1.py
+++ lab1.py
@@ -19,3 +19,8 @@ similitud1 = numerador / denominador
 numerador = pepito_sherk * Alvin_sherk
 denominador = (pepito_sherk**2)**0.5 * (Alvin_sherk**2)**0.5
 similitud2 = numerador / denominador
+
+# !Paso 3: Calcular la prediccion
+numerador = ((similitud1 * pepito_jackass) + (similitud2 * Alvin_jackass))
+denominador = (similitud1 + similitud2)
+prediccion = numerador / denominador
