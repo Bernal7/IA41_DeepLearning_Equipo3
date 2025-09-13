9a35abc1295af019c7d7acc6ae8b77c8783a12cd
JunneBa <jbanuelos391@gmail.com>
2025-09-12 22:05:51 -0700

    Jocelyn Commit

--- lab1.py
+++ lab1.py
@@ -5,3 +5,12 @@ pepito_jackass = 5
 # Ramona calificaciones
 Ramona_sherk = 1
 Ramona_jackass = None
+
+# Alvin calificaciones conocidas
+Alvin_sherk = 4
+Alvin_jackass = 2
+
+# !Paso 1: calcular la similitud entre pepito (U) y Ramona (V).
+numerador = pepito_sherk * Ramona_sherk
+denominador = (pepito_sherk**2)**0.5 * (Ramona_sherk**2)**0.5
+similitud1 = numerador / denominador
