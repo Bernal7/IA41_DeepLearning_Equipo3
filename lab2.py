7b8034f746e91e57a8a77603410cdfb99c5c6b9f
DonnieeOnieeCries <tic-320019@utnay.edu.mx>
2025-09-26 17:37:06 -0700

    Donnovan Joel Creano Rodriguez Aport

--- lab2.py
+++ lab2.py
@@ -26,3 +26,41 @@ usuario4_naruto = 3.5
 usuario4_demon = 4.5
 
 # ? Definir calificacion de los usuarios segun la pelicula
+
+
+
+
+
+# TODO Calcular Similitud #1 para usuario 1 y usuario 2
+
+numerador = usuario1_pokemon * usuario2_pokemon
+
+denominador = math.sqrt(math.pow(usuario1_pokemon, 2) * math.pow(usuario2_pokemon, 2))
+
+sim1 = numerador / denominador
+
+# TODO Calcular Similitud #1 para usuario 1 y usuario 2
+
+
+
+# TODO Calcular Similitud #2 para usuario 1 y usuario 3
+
+numerador = usuario1_naruto * usuario3_naruto
+
+denominador = math.sqrt(math.pow(usuario1_naruto, 2) * math.pow(usuario3_naruto, 2))
+
+sim2 = numerador / denominador
+
+# TODO Calcular Similitud #2 para usuario 1 y usuario 3
+
+
+
+# TODO Calcular Similitud #3 para usuario 1 y usuario 4
+
+numerador = (usuario1_pokemon * usuario4_pokemon) + (usuario1_naruto * usuario4_naruto)
+
+denominador = math.sqrt((math.pow(usuario1_pokemon, 2) + math.pow(usuario1_naruto, 2)) * (math.pow(usuario4_pokemon, 2) + math.pow(usuario4_naruto, 2)))
+
+sim3 = numerador / denominador
+
+# TODO Calcular Similitud #3 para usuario 1 y usuario 4
