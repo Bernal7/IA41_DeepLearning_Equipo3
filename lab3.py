593538e9fc7d5da977f10180a8b2495c3ca17e87
Fernangel7 <cholofernandez990@gmail.com>
2025-09-30 20:32:31 -0700

    Jose Angel Bernal Loma Aport
    Se añadieron las entradas

---
+++ lab3.py
@@ -0,0 +1,10 @@
+#INVESTIGACIONES
+#QUE ES UN PESO:
+#Son valores numericos que determinan la importancia de cada entrada en una neurona, ayudana a ajustar la influencia que tiene cada variable de entrada de salida final
+
+#QUE ES UN "BIAS"
+#Es un valor adicional que suma al resultado de la combinacion lineal de entrada y pesos. Permite desplazar la funcion de activacion, ayudando a que la neruona modele funciones mas complejas
+
+#NEURONA SIMPLE SIN ENTRENAR (PESOS ELEGIDOS A MANO) QUE SIMULE COMPUERTA LOGICA AND
+#Datos de entrada (x1, x2) -> tabla de verdad NAND
+entradas = [[0,0], [0,1], [1,0], [1,1]]
