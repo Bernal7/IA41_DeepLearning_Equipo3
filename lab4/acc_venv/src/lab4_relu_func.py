a4dc384b63a7c3d402b89a55d3874b30bb95faae
Fernangel7 <cholofernandez990@gmail.com>
2025-10-05 19:51:33 -0700

    Se añaden los archivos necesarios para lab4

---
+++ lab4/acc_venv/src/lab4_relu_func.py
@@ -0,0 +1,27 @@
+import numpy as np
+import time 
+import random
+
+#* Semilla para reproducibilidad
+rng = np.random.default_rng(42)  
+ 
+#* 3 entradas → 4 salidas
+W = rng.random((3, 4))   # Matriz de pesos 3x4
+x = np.array([1.0, 0.5, -1.0])  # Vector de entrada (3 valores)
+
+
+#* Feedforward (producto punto entrada * pesos)
+z = x @ W  # Equivalente a np.dot(x, W)
+#*np.dot()
+
+y_relu = np.maximum(0, z) # todo: establecer funcion de activacion relu
+
+#* Mostrar resultados
+print("Matriz de pesos (3x4):")
+print(W)
+print("\nEntrada:")
+print(x)
+print("\nSalida:")
+print(y_relu)
+
+
