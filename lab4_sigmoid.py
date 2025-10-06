3ed1ddc375aa21968b05cdc55581a0aced7f0359
kevingodoy0 <tic-320128@utnay.edu.mx>
2025-10-05 20:30:11 -0700

    Kevin Imanol Godoy Romo, APORT

--- lab4_sigmoid.py
+++ lab4_sigmoid.py
@@ -4,3 +4,14 @@ import random
 
 #* Semilla para reproducibilidad
 rng = np.random.default_rng(42)  
+ 
+#* 3 entradas → 4 salidas
+W = rng.random((3, 4))   # Matriz de pesos 3x4
+x = np.array([1.0, 0.5, -1.0])  # Vector de entrada (3 valores)
+
+
+#* Feedforward (producto punto entrada * pesos)
+z = x @ W  # Equivalente a np.dot(x, W)
+#*np.dot()
+
+y_sigmoid = 1 / (1 + np.exp(-z)) # todo establecer funcion de activacion sigmoid
