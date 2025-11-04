8e9f49b4b6b7050dbdece415213593ed2c907f79
#Fernangel <105268105+Fernangel7@users.noreply.github.com>
2025-11-03 19:54:49 -0700

    Create lab7.py

---
+++ lab7.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("... Bienvenido ...\n")
+print("Selecciona una función de activación para realizar las predicciones ...")
+print("1) ReLU")
+print("2) Sigmoid")
+print("3) Tangente hiperbólica\n")
+
+opt = int(input("Your Option: "))
+print()
