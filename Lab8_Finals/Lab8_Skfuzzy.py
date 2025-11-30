cfe1ea03688d61ab1c2dd77f39d4f0089926ecd7
Angel <230223825+dotAngel@users.noreply.github.com>
2025-11-30 12:18:16 -0700

    Angel's Aport

--- Lab8_Finals/Lab8_Skfuzzy.py
+++ Lab8_Finals/Lab8_Skfuzzy.py
@@ -4,3 +4,6 @@ import numpy as np
 
 #1 se define el rango del universo (temperatura de 0 a 30)
 x_temp = np.arange(0, 31, 1)
+
+#2 se define la funcion de pertenencia triangular (x, [iniciom centro, final])
+mu_frio = fuzz.trimf(x_temp, [0, 0, 20])
